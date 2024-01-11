5a9e407e11c9216890191125ebd4f5641ec7f5bd
Vikram <singhvikramaditya0702@gmail.com>
2024-01-11 15:26:00 +0530

    Added Material UI Icons

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,13 @@
 import hamburger from "../images/hamburger-menu.svg";
 import logo from "../images/logo.jpg";
-import userIcon from "../images/userIcon.jpg";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 
 import SearchBar from "./SearchBar";
+import { Button } from "@mui/material";
+import NotificationAddIcon from "@mui/icons-material/NotificationAdd";
+import CreateNewFolderIcon from "@mui/icons-material/CreateNewFolder";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Head = () => {
   const dispatch = useDispatch();
@@ -29,8 +32,26 @@ const Head = () => {
         <SearchBar />
       </div>
 
-      <div className="col-span-1">
-        <img className="h-8" alt="user-icon" src={userIcon} />
+      <div className="col-span-1 flex items-center">
+        <Button
+          variant="text"
+          startIcon={<CreateNewFolderIcon style={{ fontSize: "1.75rem" }} />}
+          className=""
+          style={{ color: "black", textTransform: "none" }}
+        ></Button>
+        <Button
+          variant="text"
+          startIcon={<NotificationAddIcon style={{ fontSize: "1.75rem" }} />}
+          className=""
+          style={{ color: "black", textTransform: "none" }}
+        ></Button>
+        <Button
+          variant="text"
+          startIcon={<AccountCircleIcon style={{ fontSize: "1.75rem" }} />}
+          className=""
+          style={{ color: "black", textTransform: "none" }}
+        ></Button>
+        {/* <img className="h-8" alt="user-icon" src={userIcon} /> */}
       </div>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,101 +1,242 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import MovieIcon from "@mui/icons-material/Movie";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import SettingsIcon from "@mui/icons-material/Settings";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import FeedbackIcon from "@mui/icons-material/Feedback";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import StreamIcon from "@mui/icons-material/Stream";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import SportsCricketIcon from "@mui/icons-material/SportsCricket";
+import SchoolIcon from "@mui/icons-material/School";
+import DiamondIcon from "@mui/icons-material/Diamond";
+import PodcastsIcon from "@mui/icons-material/Podcasts";
+import FlagIcon from "@mui/icons-material/Flag";
+
 import { Link } from "react-router-dom";
+
 const Sidebar = () => {
   return (
-    <div className="fixed mt-[5rem] px-2 pt-4  h-[100%] w-[230px] top-0 left-0 text-black overflow-y-scroll ">
+    <div className="fixed mt-[5rem] px-2 pt-4  flex flex-col h-full w-[230px] top-0 left-0 text-black overflow-y-scroll ">
       <ul>
         <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
           {" "}
-          <Link to={"/"}>Home</Link>
+          <Link to={"/"}>
+            <Button
+              variant="text"
+              startIcon={<HomeIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              Home
+            </Button>
+          </Link>
         </li>
         <li className="cursor-pointer px-4 py-1  hover:bg-gray-200 rounded-lg">
-          {" "}
-          Shorts
+          <Button
+            variant="text"
+            startIcon={<WhatshotIcon />}
+            className="pl-2"
+            style={{ color: "black", textTransform: "none" }}
+          >
+            Shorts
+          </Button>
         </li>
         <li className="cursor-pointer px-4 py-1  hover:bg-gray-200 rounded-lg">
-          {" "}
-          Live
+          <Button
+            variant="text"
+            startIcon={<StreamIcon />}
+            className="pl-2"
+            style={{ color: "black", textTransform: "none" }}
+          >
+            Live
+          </Button>
         </li>
         <li className="cursor-pointer px-4 py-1  hover:bg-gray-200 rounded-lg">
-          {" "}
-          Subscription
+          <Button
+            variant="text"
+            startIcon={<SubscriptionsIcon />}
+            className="pl-2"
+            style={{ color: "black", textTransform: "none" }}
+          >
+            Subscription
+          </Button>
         </li>
       </ul>
       <h1 className="pt-5 font-bold ">Explore</h1>
       <ul>
-        <Link to={"/category/" + "trending"}>
+        <Link to={"/category/trending"}>
           <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
-            {" "}
-            Trending
+            <Button
+              variant="text"
+              startIcon={<ExploreIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              Trending
+            </Button>
           </li>
         </Link>
-        <Link to={"/category/" + "shopping"}>
+
+        <Link to={"/category/music"}>
           <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
-            {" "}
-            Shopping
+            <Button
+              variant="text"
+              startIcon={<MusicNoteIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              Music
+            </Button>
           </li>
         </Link>
-        <Link to={"/category/" + "music"}>
+        <Link to={"/category/movies"}>
           <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
-            {" "}
-            Music
+            <Button
+              variant="text"
+              startIcon={<MovieIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              Movies
+            </Button>
           </li>
         </Link>
-        <Link to={"/category/" + "movies"}>
+        <Link to={"/category/Gaming"}>
           <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
-            {" "}
-            Movies
+            <Button
+              variant="text"
+              startIcon={<SportsEsportsIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              Gaming
+            </Button>
           </li>
         </Link>
-        <Link to={"/category/" + "Gaming"}>
+        <Link to={"/category/news"}>
           <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
-            {" "}
-            Gaming
+            <Button
+              variant="text"
+              startIcon={<NewspaperIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              News
+            </Button>
           </li>
         </Link>
-        <Link to={"/category/" + "news"}>
+        <Link to={"/category/Sports"}>
           <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
-            {" "}
-            News
+            <Button
+              variant="text"
+              startIcon={<SportsCricketIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              Sports
+            </Button>
           </li>
         </Link>
-        <Link to={"/category/" + "Sports"}>
-          <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
+        <Link to={"/category/learning"}>
+          <li className="px-4 py-1 hover:bg-gray-200 rounded-lg">
             {" "}
-            Sports
+            <Button
+              variant="text"
+              startIcon={<SchoolIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              Learning
+            </Button>
+          </li>{" "}
+        </Link>
+        <Link to={"/category/fashion"}>
+          <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
+            <Button
+              variant="text"
+              startIcon={<DiamondIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              Fashion & Beauty
+            </Button>
           </li>
         </Link>
-        <Link to={"/category/" + "learning"}>
-          <li className="px-4 py-1 hover:bg-gray-200 rounded-lg"> Learing</li>{" "}
-        </Link>
-        <Link to={"/category/" + "fashion"}>
+        <Link to={"/category/podcast"}>
           <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
-            Fashion & Beauty
-          </li>
-        </Link>
-        <Link to={"/category/" + "podcast"}>
-          <li className="cursor-pointer px-4 py-1 hover:bg-gray-200 rounded-lg">
-            {" "}
-            Podcast
+            <Button
+              variant="text"
+              startIcon={<PodcastsIcon />}
+              className="pl-2"
+              style={{ color: "black", textTransform: "none" }}
+            >
+              Podcast
+            </Button>
           </li>
         </Link>
       </ul>
 
       <div className="border border-black my-2"></div>
       <ul>
-        <li className="px-4 py-1 hover:bg-gray-200 rounded-lg"> Setting</li>{" "}
         <li className="px-4 py-1 hover:bg-gray-200 rounded-lg">
-          Report History
+          {" "}
+          <Button
+            variant="text"
+            startIcon={<SettingsIcon />}
+            className="pl-2"
+            style={{ color: "black", textTransform: "none" }}
+          >
+            Setting
+          </Button>
+        </li>{" "}
+        <li className="px-4 py-1 hover:bg-gray-200 rounded-lg">
+          <Button
+            variant="text"
+            startIcon={<FlagIcon />}
+            className="pl-2"
+            style={{ color: "black", textTransform: "none" }}
+          >
+            Report History
+          </Button>
         </li>
-        <li className="px-4 py-1 hover:bg-gray-200 rounded-lg"> Help</li>
         <li className="px-4 py-1 hover:bg-gray-200 rounded-lg">
-          Send Feedback
+          {" "}
+          <Button
+            variant="text"
+            startIcon={<HelpOutlineIcon />}
+            className="pl-2"
+            style={{ color: "black", textTransform: "none" }}
+          >
+            Help
+          </Button>
+        </li>
+        <li className="px-4 py-1 hover:bg-gray-200 rounded-lg">
+          <Button
+            variant="text"
+            startIcon={<FeedbackIcon />}
+            className="pl-2"
+            style={{ color: "black", textTransform: "none" }}
+          >
+            Feedback
+          </Button>
+        </li>
+        <li className="px-4 py-1 hover:bg-gray-200 rounded-lg">
+          <Button
+            variant="text"
+            startIcon={<FeedbackIcon />}
+            className="pl-2"
+            style={{ color: "black", textTransform: "none" }}
+          >
+            Vikramaitya Singh
+          </Button>
         </li>
       </ul>
-
-      <div className="mt-4">
-        <p>@copyright clone By Vikrmaditya Singh</p>
-      </div>
     </div>
   );
 };
